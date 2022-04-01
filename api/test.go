a203f1092f666b23905b107cfdb926838f533d4e
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-01 16:47:05 -0500

    Serve html

--- api/test.go
+++ api/test.go
@@ -1,15 +1,9 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func Test(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprintf(w, "<h1>Whats up</h1")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
+func Test(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "./static/index.html")
 }
