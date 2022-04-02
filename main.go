8b4a2408e7c90c1badec644f834addbd72617c57
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-02 08:44:41 -0500

    Start website

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	handler "github.com/drew-harris/spotify-stopper-api/api"
+	"github.com/go-redis/redis/v8"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", handler.Test)
+var ctx = context.Background()
 
-	err := http.ListenAndServe(":8080", nil)
+func main() {
+	// Set up redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-14793.c124.us-central1-1.gce.cloud.redislabs.com:14793",
+		Password: "Pr2soSboKbJbbQv8KHwf2urwJyuk5Yyh",
+		DB:       0,
+	})
+
+	err := rdb.Set(ctx, "test", "test", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "test").Result()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(val)
+	// Set up http
+	http.HandleFunc("/", handler.Test)
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
