fdb9751b68823ed6948dc6dc9aec092d452dbdae
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-02 10:32:25 -0500

    Functionality

--- go.mod
+++ go.mod
@@ -6,4 +6,11 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/zmb3/spotify/v2 v2.0.1 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,32 +5,81 @@ import (
 	"fmt"
 	handler "github.com/drew-harris/spotify-stopper-api/api"
 	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+	"golang.org/x/oauth2"
+	"log"
 	"net/http"
+	"os"
 )
 
 var ctx = context.Background()
 
+var client *spotify.Client
+
 func main() {
 	// Set up redis
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis-14793.c124.us-central1-1.gce.cloud.redislabs.com:14793",
-		Password: "Pr2soSboKbJbbQv8KHwf2urwJyuk5Yyh",
+		Addr:     os.Getenv("REDIS_URI"),
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 
-	err := rdb.Set(ctx, "test", "test", 0).Err()
-	if err != nil {
-		panic(err)
+	auth := spotifyauth.New(spotifyauth.WithRedirectURL(os.Getenv("CALLBACK")),
+		spotifyauth.WithScopes("user-read-currently-playing", "user-read-playback-state", "user-modify-playback-state"))
+
+	url := auth.AuthURL("")
+
+	tokenString := rdb.Get(ctx, "access_token").Val()
+	refreshString := rdb.Get(ctx, "refresh_token").Val()
+
+	// Create oauth 2 token
+	token := &oauth2.Token{
+		AccessToken:  tokenString,
+		RefreshToken: refreshString,
 	}
 
-	val, err := rdb.Get(ctx, "test").Result()
-	if err != nil {
-		panic(err)
+	client = spotify.New(auth.Client(ctx, token))
+
+	spotifyHandleCallback := func(w http.ResponseWriter, r *http.Request) {
+		tok, err := auth.Token(ctx, "", r)
+
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err)
+			return
+		}
+
+		fmt.Fprintf(w, "Access token: %s", tok.AccessToken)
+
+		rdb.Set(ctx, "access_token", tok.AccessToken, 0)
+		rdb.Set(ctx, "refresh_token", tok.RefreshToken, 0)
+
+		client = spotify.New(auth.Client(ctx, tok))
 	}
 
-	fmt.Println(val)
-	// Set up http
+	pause := func(w http.ResponseWriter, r *http.Request) {
+		err := client.Pause(ctx)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Error: %s", err)
+			return
+		}
+		// Set status 200
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "Paused")
+	}
+
+	fmt.Println(url)
+
 	http.HandleFunc("/", handler.Test)
+	http.HandleFunc("/callback", spotifyHandleCallback)
+	http.HandleFunc("/pause", pause)
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
