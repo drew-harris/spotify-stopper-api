66690b8e5663e7fc7743325f2f685e5ec6fba82d
Drew Harris <61394759+drew-harris@users.noreply.github.com>
2022-04-01 20:25:37 -0500

    Use templating

--- api/test.go
+++ api/test.go
@@ -1,9 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 )
 
-func Test(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, "./static/index.html")
+type Page struct {
+	Message string `json:"message"`
+}
+
+func Test(w http.ResponseWriter, _ *http.Request) {
+	t, err := template.ParseFiles("./api/hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	p := Page{Message: "Hello World"}
+
+	err = t.Execute(w, p)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
